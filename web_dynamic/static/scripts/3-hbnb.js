0283057d364b4e5e0db162cd9ebc0f6283a56e7b
Slamchillz <mendyslam@gmail.com>
2022-10-30 00:59:13 +0000

    building script3 on script2

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
-  const url = 'http://0.0.0.0:5001/api/v1/places_search';
+  const selectedAmenities = {};
+  const amenity = $('div.amenities input[type="checkbox"]');
+  amenity.change(function () {
+    const id = $(this).attr('data-id');
+    const name = $(this).attr('data-name');
+    if ($(this).is(':checked')) {
+      selectedAmenities[id] = name;
+    } else {
+      delete selectedAmenities[id];
+    }
+    const amenities = Object.values(selectedAmenities);
+    $('div.amenities > h4').text(amenities.join(', '));
+  });
+
+  const statusUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  $.ajax({
+    url: statusUrl,
+    type: 'GET',
+    dataType: 'json'
+  })
+    .done(function (response) {
+      console.log(response);
+      if (response.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    })
+    .fail(function (xhr, status, error) {
+      console.log('Error', error);
+    });
+
+  const searchUrl = 'http://0.0.0.0:5001/api/v1/places_search';
   const post = $.ajax({
-    url: url,
+    url: searchUrl,
     data: JSON.stringify({}),
     contentType: 'application/json',
     type: 'POST',
@@ -55,11 +87,11 @@ function populateInfomation (place) {
 
 function populateUser (place) {
   const userId = place.user_id;
-  const url = 'http://0.0.0.0:5001/api/v1/users/' + userId;
+  const userUrl = 'http://0.0.0.0:5001/api/v1/users/' + userId;
   const divUser = $('<div></div>').addClass('user');
   divUser.append($('<b></b>').text('Owner: '));
   const getUser = $.ajax({
-    url: url,
+    url: userUrl,
     type: 'GET',
     dataType: 'json',
     ContentType: 'application/json'
